21b16d367ffe630da89534b4915202e60c551ca7
William Ma <williamjma@hotmail.com>
2014-11-02 00:38:21 -0400

    blocking bug fixes
    fixed issues where youtube,facebook and a lot of javascript breaks with
    this function. also, links don't break

--- replacer.js
+++ replacer.js
@@ -1,6 +1,17 @@
+jQuery.fn.textWalk = function( fn ) {
+    this.contents().each( jwalk );
+    
+    function jwalk() {
+        var name = this.nodeName.toLowerCase();
+        if( name === '#text' ) {
+            fn.call( this );
+        } else if( this.nodeType === 1 && this.childNodes && this.childNodes[0] && name !== 'script' && name !== 'textarea' ) {
+            $(this).contents().each( jwalk );
+        }
+    }
+    return this;
+};
 
-//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("the", "g"), "DARN");
-$(document).ready(function() {
-	alert("aa");
+$('body').textWalk(function() {
+    this.data = this.data.replace('the','Choppable Beast Dweep');
 });
-//it doesnt work all the time
