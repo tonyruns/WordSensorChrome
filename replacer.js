4ab68a35e9b141c30478b547667fd5cc8272eafe
tonyruns <tjin.1995@gmail.com>
2014-11-02 01:45:11 -0500

    Filtering
    Updated blacklist of words

--- replacer.js
+++ replacer.js
@@ -11,6 +11,7 @@ jQuery.fn.textWalk = function( fn ) {
     return this;
 };
 var filter = function() {
+    this.data = this.data.replace(/blowjob/ig, 'sexual act');
     this.data = this.data.replace(/fuck/ig,'frig');
     this.data = this.data.replace(/damn/ig,'darn');
     this.data = this.data.replace(/nigger/ig,'negro');
@@ -19,11 +20,12 @@ var filter = function() {
     this.data = this.data.replace(/fag/ig,'homosexual');
     this.data = this.data.replace(/bitch/ig,'female dog');
     this.data = this.data.replace(/cunt/ig,'vagina');
+    this.data = this.data.replace(/drunk/ig, 'blood alcohol content over 0.08%')
     this.data = this.data.replace(/gay/ig,'homosexual');
     this.data = this.data.replace(/hell/ig,'heck');
     this.data = this.data.replace(/kunt/ig,'vagina');
     this.data = this.data.replace(/lesbian/ig,'homosexual');
-
+    this.data = this.data.replace(/piss/ig, 'urine');
     this.data = this.data.replace(/pussy/ig,'female reproductive organ');
     this.data = this.data.replace(/queer/ig,'homosexual');
     this.data = this.data.replace(/slut/ig,'sexually popular woman');
@@ -32,8 +34,6 @@ var filter = function() {
     this.data = this.data.replace(/shit/ig,'poop');
     this.data = this.data.replace(/tit/ig,'breast');
     this.data = this.data.replace(/tits/ig,'breast');
-    this.data = this.data.replace(/testicle/ig,'male genitalia');
-    this.data = this.data.replace(/vajina/ig,'female genitalia');
     this.data = this.data.replace(/whore/ig,'hussy');
 };
 $('body').textWalk(filter);
