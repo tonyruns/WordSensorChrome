370d9759da7aac277265862e75b666fdca39844e
Dweep Shah <dweepcan1@msn.com>
2014-11-02 01:36:19 -0500

    Added swear words

--- replacer.js
+++ replacer.js
@@ -12,7 +12,17 @@ jQuery.fn.textWalk = function( fn ) {
 };
 
 var filter = function() {
-    this.data = this.data.replace(/the shit/ig,'the poo');
+    this.data = this.data.replace(/pussy/ig,'female reproductive organ');
+    this.data = this.data.replace(/queer/ig,'homosexual');
+    this.data = this.data.replace(/slut/ig,'sexually popular woman');
+    this.data = this.data.replace(/skank/ig,'dirty girl');
+    this.data = this.data.replace(/shitty/ig,'bad');
+    this.data = this.data.replace(/shit/ig,'poop');
+    this.data = this.data.replace(/tit/ig,'breast');
+    this.data = this.data.replace(/tits/ig,'breast');
+    this.data = this.data.replace(/testicle/ig,'male genitalia');
+    this.data = this.data.replace(/vajina/ig,'female genitalia');
+    this.data = this.data.replace(/whore/ig,'hussy');
 };
 
 $('body').textWalk(filter);
