5b49c70e99c6a135e4c0b0284066bb44f75e10c8
William Ma <williamjma@hotmail.com>
2014-11-02 13:55:44 -0500

    addition function
    attempted to add enable disable

--- replacer.js
+++ replacer.js
@@ -1,4 +1,4 @@
-jQuery.fn.textWalk = function( fn ) {
+jQuery.fn.textWalk = function( flag ) {
     this.contents().each( jwalk );
     function jwalk() {
         var name = this.nodeName.toLowerCase();
@@ -6,7 +6,7 @@ jQuery.fn.textWalk = function( fn ) {
             replaceData(this);
         } else if (name === 'input') {
             replacePlaceholder(this);
-            if (this.getAttribute("type")==='submit'){
+            if (this.getAttribute("type")==='submit' || flag){
                 replaceValue(this);
             }
             
@@ -73,28 +73,37 @@ function replaceValue(elem) {
 
 
 var elemLength = 0;
+var enable = false;
 $(document).ready(function() {
  /*   chrome.storage.local.clear(function(result) {
         alert('cleared');
     });*/
     chrome.storage.local.get(function (result) {
         elemLength = result['1'];
+        enable = result['2'];
         if (typeof elemLength === 'undefined')
             elemLength = 0;
+        if (typeof enable === 'undefined')
+            enable = true;
         var fixedLength = blacklist.length + elemLength;
         for (var i=blacklist.length; i < fixedLength; i++)
         {
             blacklist.push([new RegExp('\\b' + result[i][0] + '\\b','ig'),result[i][1]]);
         }
     });
-    $('body').textWalk();
+
+    if (!enable) {
+        $('body').textWalk(true);
+    }
 });
 
 
 // Create a MutationObserver to handle events
 // (e.g. filtering TextNode elements)
 var observer = new MutationObserver(function(mutations) {
-    $('body').textWalk();
+    if (!enable) {
+        $('body').textWalk(false);
+    }
 });
 
 // Start observing "childList" events in document and its descendants
@@ -103,7 +112,7 @@ observer.observe(document, {
     subtree:   true
 });
 
-$('#test').click(function() {
+$('#addition').click(function() {
     var replacedWord = $('#replacedWord').val();
     var replacingWord = $('#replacingWord').val();
     var dataObj = {};
@@ -112,8 +121,19 @@ $('#test').click(function() {
     dataObj[thisname] = elemLength + 1;
     chrome.storage.local.set(dataObj, function() {
         blacklist.push([new RegExp('\\b' + replacedWord + '\\b','ig'),replacingWord]);
+        chrome.tabs.reload(function(){});
     });
 //    blacklist[blacklist.length][0] = replacedWord;
 //    blacklist[blacklist.length][1] = replacingWord;
     
+});
+
+$('#check').change(function() {
+    var val = $('#check').is(':checked');
+    var dataObj = {};
+    var enabled = 2;
+    dataObj[enabled] = val;
+    chrome.storage.local.set(dataObj, function() {
+        chrome.tabs.reload(function(){});
+    });
 });
