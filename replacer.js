6df59af262eeaaa5a5fdf1a801be1a11e49a0be5
William Ma <williamjma@hotmail.com>
2014-11-02 00:25:02 -0400

    jQuery addition
    added jquery function

--- replacer.js
+++ replacer.js
@@ -1,2 +1,6 @@
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("the", "g"), "William Chops");
+
+//document.body.innerHTML = document.body.innerHTML.replace(new RegExp("the", "g"), "DARN");
+$(document).ready(function() {
+	alert("aa");
+});
 //it doesnt work all the time
